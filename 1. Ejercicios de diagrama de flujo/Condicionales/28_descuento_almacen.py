1b67c1b768af5874c939ed80561800cbf6bd0e42
carlalopxz <carlalopezprogamacion@gmail.com>
2021-05-08 20:58:29 -0300

    ejercicios nuevos

--- 1. Ejercicios de diagrama de flujo/Condicionales/28_descuento_almacen.py
+++ 1. Ejercicios de diagrama de flujo/Condicionales/28_descuento_almacen.py
@@ -1,1 +1,8 @@
-print('Ingrese el valor de la commpra')
+compra = int(input('Ingrese el valor de la compra \n'))
+
+if compra > 1000:
+    descuento = compra * 0.20
+    precioFinal = compra - descuento
+    print(f'el precio final con el descuento es de {precioFinal} pesos')
+else: 
+    print(f'No se hace ningun descuento')
